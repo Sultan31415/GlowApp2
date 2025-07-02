2cb6a18ccd9fd0616f3d489b272b95edd2dd9a27
Sultan31415 <sultanyermakhan@gmail.com>
2025-07-02 13:16:25 +0500

    Debugging for https

--- front/src/utils/useApi.ts
+++ front/src/utils/useApi.ts
@@ -13,8 +13,8 @@ export const useApi = () => {
             },
         };
 
-        const API_BASE_URL = import.meta.env.VITE_API_URL || 'http://oylan.me/api';
-        const response = await fetch(`${API_BASE_URL}/${endpoint}`, {
+        const API_BASE_URL = import.meta.env.VITE_API_URL || 'https://oylan.me';
+        const response = await fetch(`${API_BASE_URL}/api/${endpoint}`, {
             ...defaultOptions,
             ...options,
             headers: {
